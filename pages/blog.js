3d909f97eaf3d10eeb1a768c8c60e285243a74fa
Sirena Gandy <sirena@sirenaalyce.com>
2022-01-27 23:40:24 -0500

    created blog main page and dynamic route pages

---
+++ pages/blog.js
@@ -0,0 +1,15 @@
+import NavBar from "../components/NavBar";
+
+export default function Blog() {
+  return (
+    <div className="w-screen h-full text-white">
+      <NavBar />
+      <div className="grid border-2 border-white h-56 content-center">
+        <h2 className="text-pink-600 text-9xl font-extrabold">Blog</h2>
+      </div>
+      <div>
+          Blog posts
+      </div>
+    </div>
+  );
+}
