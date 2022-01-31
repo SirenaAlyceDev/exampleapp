474bdb5a257932ff1332096cd00fc05bb28869ef
Sirena Gandy <sirena@sirenaalyce.com>
2022-01-31 16:07:21 -0500

    set dynamic pages for individual posts

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,57 @@
 import NavBar from "../../components/NavBar";
+import { GraphQLClient, gql } from "graphql-request";
 
-export default function Blog() {
+const graphcms = new GraphQLClient(
+  "https://api-us-east-1.graphcms.com/v2/ckyjg354o11vl01yy3in52e9d/master"
+);
+
+export const QUERY = gql`
+  query Post($slug: String!) {
+    blogPost(where: { slug: $slug }) {
+      id
+      slug
+      title
+      author {
+        name
+      }
+      content {
+        html
+      }
+    }
+  }
+`;
+
+export const SLUGLIST = gql`
+  {
+    blogPosts {
+      slug
+    }
+  }
+`;
+
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+
+  const blogPost = await graphcms.request(QUERY, { slug });
+
+  const post = blogPost.blogPost;
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+export async function getStaticPaths() {
+  const { blogPosts } = await graphcms.request(SLUGLIST);
+
+  return {
+    paths: blogPosts.map((post) => ({ params: { slug: post.slug } })),
+    fallback: "blocking",
+  };
+}
+
+export default function Blog({ post }) {
   return (
     <div className="w-screen h-full text-white">
       <NavBar />
@@ -8,7 +59,9 @@ export default function Blog() {
         <h2 className="text-pink-600 text-9xl font-extrabold">Blog</h2>
       </div>
       <div>
-          individual blog post
+        <h2 className="text-pink-600 font-extrabold text-2xl">{post.title}</h2>
+        <p>{post.author.name}</p>
+        <div dangerouslySetInnerHTML={{ __html: post.content.html }} />
       </div>
     </div>
   );
